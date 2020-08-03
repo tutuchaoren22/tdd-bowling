8bfca0aa61ff4773cba8b5c04ccf194de71683f5
duyuqian <809009017@qq.com>
2020-08-03 22:09:41 +0800

    [duyuqian] feat: return sum of number when no strike and spare

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -14,10 +14,20 @@ public class BowlingGameTest {
     }
 
     @Test
-    public void should_return_zero_when_given__zero() {
-        int[] allZeroScore = new int[20];
+    public void should_return_zero_when_given_zero() {
+        int[] allZeroNumber = new int[20];
+        bowlingGame.setThrowNumber(allZeroNumber);
         int result = bowlingGame.getScore();
         int expected = 0;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void should_return_sum_of_number_when_no_strike_and_spare() {
+        int[] noStrikeAndSpareNumber = {1,3,4,5,5,2,1,2,4,2,3,6,4,1,2,5,3,4,2,6};
+        bowlingGame.setThrowNumber(noStrikeAndSpareNumber);
+        int result = bowlingGame.getScore();
+        int expected = 65;
         assertEquals(result, expected);
     }
 
